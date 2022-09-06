d00769ebee61459131c1d7cd020751258d4473cf
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-09-06 14:41:19 +0000

    Add string import and upper case alphabet

--- run.py
+++ run.py
@@ -1,13 +1,40 @@
 import random
 from countries import country_list
 import alien
+import string
 
 game_finished = False
 remaining_attempts = len(alien.ALIENS)
 display = []
+alphabet = string.ascii_uppercase
+
 
 print(alien.LOGO)
-print("        WELCOME TO ALIEN INVASION..!\n")
+
+def intro():
+    """
+    Welcome message introducing the name of the game.
+    Welcome text and instructions for gameplay.
+    Enter name input to start game.
+    """
+    print("        WELCOME TO ALIEN INVASION...!\n")
+    print("Hello Earthling, I am from the planet Aldaran...\n")
+    print("We have been watching Earth for many years...\n")
+    print("You have not been doing a good job of keeping it safe...\n")
+    print("So we are here to take over...\n")
+    print("If you want to stop us from invading...\n")
+    print("You need to guess which country we intend to invade first.\n")
+    print("But beware. you can only guess wrong 6 times...\n")
+    print("Before you start you need to tell us your name.\n")
+    
+    user = input("What is your name Earthling?\n")
+
+    print(f"Greetings {user}...\n")
+
+    return user
+
+
+intro()    
 
 def select_random_country(country_list):
     """
@@ -30,9 +57,9 @@ while not game_finished:
     if guess in display:
         print(f"You've already guessed {guess}, Choose again...")
 
-    if guess not in 'abcdefghijklmnoprstuvwxyz':
+    if guess not in alphabet:
         print("Please enter a LETTER.")
-   
+
 
     for position in range(country_length):
         letter = random_country[position]
