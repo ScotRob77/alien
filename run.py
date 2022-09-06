1643fbdac4c1fa35a45250904ea988213b564213
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-09-06 13:08:46 +0000

    Add function to create random country

--- run.py
+++ run.py
@@ -8,3 +8,16 @@ display = []
 
 print(alien.LOGO)
 print("        WELCOME TO ALIEN INVASION..!\n")
+
+def select_random_country(country_list):
+    """
+    Will randomly select a country from the list of countries
+    """
+    return random.choice(country_list)
+print(select_random_country(country_list))
+
+random_country = select_random_country(country_list)
+country_length = len(random_country)
+
+for _ in range(country_length):
+    display += " _ "
