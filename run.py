b0db3ead236acd1d9d5b9a17d57fa1acdc9f6f65
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-09-06 13:33:11 +0000

    Ad while loop to loop through game

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from countries import country_list
 import alien
 
 game_finished = False
-remaining_attempts = len(alien.ALIENS) - 1
+remaining_attempts = len(alien.ALIENS)
 display = []
 
 print(alien.LOGO)
@@ -20,4 +20,31 @@ random_country = select_random_country(country_list)
 country_length = len(random_country)
 
 for _ in range(country_length):
-    display += " _ "
+    display += "_"
+
+while not game_finished:
+    guess = input("Guess a letter: ").strip()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(country_length):
+        letter = random_country[position]
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+
+    if guess not in random_country:
+        print(f"You guessed {guess}, that's not in the country...")
+        print("You lose a life...")
+        remaining_attempts -= 1
+        if remaining_attempts == 0:
+            game_finished = True
+            print("Game Over.... You lose..!")
+
+        if "_" not in display:
+            game_finished = True
+            print("You Won")
+
+        print(alien.ALIENS[remaining_attempts])
+        
