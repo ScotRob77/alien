034c8346fb07ca99c601d50f6987b4980eaf5054
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-09-08 02:52:58 +0000

    Tidy up code so game starts correctly

--- run.py
+++ run.py
@@ -12,22 +12,6 @@ alphabet = string.ascii_uppercase
 print(alien.LOGO)
 
 
-def rules():
-    """
-    Welcome message introducing the name of the game.
-    Welcome text and instructions for gameplay.
-    Enter name input to start game.
-    """
-    print("        WELCOME TO ALIEN INVASION...!\n")
-    print("Hello Earthling, we are from the planet Aldaran...\n")
-    print("We have been watching Earth for many years...\n")
-    print("You have not been doing a good job of keeping it safe...\n")
-    print("So we are here to take over...\n")
-    print("If you want to stop us from invading...\n")
-    print("You need to guess which country we intend to invade first.\n")
-    print("But beware. you can only guess wrong 6 times...\n")
-    
-
 def start_game():
     print("Before you start you need to tell us your name.\n")
     
@@ -39,8 +23,28 @@ def start_game():
     return user
 
 
+def rules():
+    """
+    Welcome message introducing the name of the game.
+    Welcome text and instructions for gameplay.
+    Enter name input to start game.
+    """
+    print(alien.LOGO)
+    print("        WELCOME TO ALIEN INVASION...!\n")
+    print("Hello Earthling, we are from the planet Aldaran...\n")
+    print("We have been watching Earth for many years...\n")
+    print("You have not been doing a good job of keeping it safe...\n")
+    print("So we are here to take over...\n")
+    print("If you want to stop us from invading...\n")
+    print("You need to guess which country we intend to invade first.\n")
+    print("But beware. you can only guess wrong 6 times...\n")
+    print("Let's start the game....\n")
+    start_game()
+    
+
 def intro():
-    print("Hello Earthling press 1 for rules or 2 to play game")
+    print("             ALIEN INVASION...!\n")
+    print("Hello Earthling.\nPress 1 for rules or 2 to play game")
     new_user = input("Enter 1 or 2: ")
     if new_user == "1":
         rules()
@@ -59,6 +63,7 @@ def select_random_country(country_list):
 
 random_country = select_random_country(country_list)
 country_length = len(random_country)
+
 
 for _ in range(country_length):
     display += "_"
